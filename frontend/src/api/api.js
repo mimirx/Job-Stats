818d4e1204ff356ae5b8f7688413cbd65f0bd457
mimirx <maimarinov@gmail.com>
2026-03-11 22:22:36 -0500

    Connect frontend to production backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL: "https://job-stats-905b.onrender.com"
 })
 
 api.interceptors.request.use(config => {
